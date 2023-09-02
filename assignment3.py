06ff0fe5e0bd694d70570b03dc5df91a947899dd
Hasibmihad <mdhasibuddin12@gmail.com>
2023-09-02 14:14:22 +0600

    datadict

--- assignment3.py
+++ assignment3.py
@@ -12,14 +12,19 @@ def downloadData(url):
 
 url_data = downloadData("http://s3.amazonaws.com/cuny-is211-spring2015/weblog.csv")
 csv_data = csv.reader(io.StringIO(url_data))
+dataDict={}
+row_id=0
 for row in csv_data:
         # Extract values for each column
-        path_to_file = row[0]
+
+        path = row[0]
         datetime_accessed = row[1]
         browser = row[2]
-        status_of_request = row[3]
-        request_size_bytes = row[4]
-        
-        # Perform any processing or analysis on the data here
-        # For example, you can print the values for each row
-        print(f"Path: {path_to_file}, Datetime: {datetime_accessed}, Browser: {browser}, Status: {status_of_request}, Size: {request_size_bytes}")
+        # i need only time 
+        time_accessed= datetime_accessed.split(' ')
+        time=time_accessed[1]
+
+        dataDict[int(row_id)]= (path,time,browser)
+        row_id=row_id+1
+
+print(dataDict)
