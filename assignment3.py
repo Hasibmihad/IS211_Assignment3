5ead8814430a983a3cc57e1e2d37461dc1fdc540
Hasibmihad <mdhasibuddin12@gmail.com>
2023-09-02 13:44:31 +0600

    data

--- assignment3.py
+++ assignment3.py
@@ -13,4 +13,13 @@ def downloadData(url):
 url_data = downloadData("http://s3.amazonaws.com/cuny-is211-spring2015/weblog.csv")
 csv_data = csv.reader(io.StringIO(url_data))
 for row in csv_data:
-    print(row)
+        # Extract values for each column
+        path_to_file = row[0]
+        datetime_accessed = row[1]
+        browser = row[2]
+        status_of_request = row[3]
+        request_size_bytes = row[4]
+        
+        # Perform any processing or analysis on the data here
+        # For example, you can print the values for each row
+        print(f"Path: {path_to_file}, Datetime: {datetime_accessed}, Browser: {browser}, Status: {status_of_request}, Size: {request_size_bytes}")
